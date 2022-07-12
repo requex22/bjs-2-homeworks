9b450aa3026672f2dc65d829b968102c820d3804
Stanislav <stas_sokolov_2001@mail.ru>
2022-07-12 14:30:20 +0600

    hw done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,22 @@
+"use strict"
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+
+  if (a === 0) {
+	arr[0] = -c / b;
+	return arr;
+  }
+
+  let discriminant = Math.pow(b, 2) - 4 * a * c; 
+
+  if (discriminant > 0) {
+	arr[0] = parseInt((( -b + Math.sqrt(discriminant)) / (2 * a)).toFixed(0));
+	arr[1] = parseInt((( -b + Math.sqrt(discriminant)) / (2 * a)).toFixed(0));
+  } else if (discriminant === 0) {
+	arr[0] = -b / (2 * a);
+  }
+  
   return arr; // array
 }
 
