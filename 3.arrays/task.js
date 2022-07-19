b36857857a09c471b4bc58ae7750c79c9e4c3080
Stanislav <stas_sokolov_2001@mail.ru>
2022-07-19 15:19:33 +0600

    hw in arrays added

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,13 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  let result = arr1.every((value, index) => value === arr2[index] && arr1.length === arr2.length);
+  return result;
 }
+
+console.log(compareArrays([6,2,3],[6,2,3]))
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  let resultArr = arr.filter(element => element > 0 && element % 3 === 0).map(element => element * 10)
+  return resultArr; 
 }
+
+console.log(advancedFilter([2, 4, 5, 6, 9, 10]))
