1688bc62f0d8254203d215ed7b86d9e93eaa8828
Stanislav <stas_sokolov_2001@mail.ru>
2022-08-16 12:21:15 +0600

    dz

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,53 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+	let cache = [];
+
+	function wrapper(...args) {
+		const hash = args.join(","); // получаем правильный хэш
+		console.log(`Принимаем новую порцию хеша: ${hash}`);
+		console.log(`Текущий размер кеша: ${cache.length}`);
+
+		let idx = cache.findIndex((item) => item.hash === hash) // ищем элемент, хэш которого равен нашему хэшу
+
+		if (idx == -1 ) { // если элемент НЕ найден
+			console.log(`В кэше результата нет - придётся считать`);
+			let result = func(...args); // в кэше результата нет - придётся считать
+			cache.push({hash, result}); // добавляем элемент с правильной структурой
+			console.log(`Производим вычисление: ${result}`);
+			console.log("Добавляем вычисленный результат в кеш");
+
+			if (cache.length > 5) { 
+				cache.splice(0, 1); // если слишком много элементов в кэше надо удалить самый старый (первый)
+				console.log("Кеш переполнен, удаляем самый старый кеш");
+			}
+
+			return "Вычисляем: " + result; 
+		}
+
+		console.log(`Найден кеш. Результат из кэша: ${cache[idx].result}`)// индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+		return "Из кэша: " + cache[idx].result;
+	}
+	
+	return wrapper;
 }
 
 
 function debounceDecoratorNew(func) {
-  // Ваш код
+	let timeout;
+    let time = Date.now();
+
+    return function (...args) {
+    
+        clearTimeout(timeout);
+        
+        timeout = setTimeout(() => {
+            timeout = null;
+            func.apply(this, args);
+            console.log(Date.now() - time + " мс");
+        }, ms);
+
+    };
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
-}
+// function debounceDecorator2(func) {
+//   // Ваш код
+// }
